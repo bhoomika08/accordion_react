40d5368d5ad5036df153b42439f87cf6798f7c74
Bhoomika <bhoomika@vinsol.com>
2020-01-31 01:30:06 +0530

    change the accordion data array

--- src/App.js
+++ src/App.js
@@ -5,30 +5,44 @@ import './App.css';
 const products = [
   {
     label: 'label1',
-    title: 'Title 1',
-    detail: 'Detail 1', 
+    title: 'Collapsible Item #1',
+    detail: `Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
+    3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. 
+    Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. 
+    Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. 
+    Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you 
+    probably haven't heard of them accusamus labore sustainable VHS.`, 
   },
   {
     label: 'label2',
-    title: 'Title 2',
-    detail: 'Detail 2', 
+    title: 'Collapsible Item #2',
+    detail: `Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
+    3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. 
+    Brunch 3 wolf moon tempor, sunt aliqua putnpm a bird on it squid single-origin coffee nulla assumenda shoreditch et. 
+    Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident 
+    probably haven't heard of them accusamus labore sustainable VHS.`, 
   },
   {
     label: 'label3',
-    title: 'Title 3',
-    detail: 'Detail 3', 
+    title: 'Collapsible Item #3',
+    detail: `Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 
+    3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. 
+    Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you 
+    probably haven't heard of them accusamus labore sustainable VHS.`, 
   },
   {
     label: 'label4',
-    title: 'Title 4',
-    detail: 'Detail 4', 
+    title: 'Collapsible Item #4',
+    detail: `Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid.
+    Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you 
+    probably haven't heard of them accusamus labore sustainable VHS.`,  
   },
 ]
 
 function App() {
   return (
     <div className='App'>
-      <Accordion allowMultipleOpen={true}>
+      <Accordion allowMultipleOpen={false}>
         {products.map((product, index) => {
           return (
           <div key={index} label={product.label} title={product.title}>
